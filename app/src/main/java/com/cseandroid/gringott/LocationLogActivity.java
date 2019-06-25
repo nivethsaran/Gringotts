21d7c5d668c7cbd7dc27761dd6915a1a23893c28
Niveth Saran <nivethsaran@gmail.com>
2019-06-25 22:17:12 +0530

    Add all activities

---
+++ app/src/main/java/com/cseandroid/gringott/LocationLogActivity.java
@@ -0,0 +1,14 @@
+package com.cseandroid.gringott;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_log);
+    }
+}
